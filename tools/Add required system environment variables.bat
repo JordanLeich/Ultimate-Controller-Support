c3c50d1383982fbe750d901cfb6fee231d654c9b
ak2yny <92672223+ak2yny@users.noreply.github.com>
2023-11-29 16:42:51 +0100

    Update Add required system environment variables.bat
    - Changed getting PATH to only get the machine path (in CMD, this only works from the registry).
    - Now checks each new path individually and also includes paths that have a trailing backslash.
    - Now uses case insensitive search, because paths are case insensitive on Windows.
    - Using `Find` instead of `Findstr /C:`, which is slightly faster.
    - Separation semicolons are all added in `%newPath%` instead of a separate one in the `setx` command.
    - Added code to remove semicolon duplicates, in case Path starts with a semicolon already (or has other semicolon duplicates).

--- tools/Add required system environment variables.bat
+++ tools/Add required system environment variables.bat
@@ -12,16 +12,20 @@ if %errorLevel% == 0 (
 )
 
 REM Check if the paths already exist in the system environment variable "Path"
-set "newPath=C:\Windows\system32;C:\Windows\System32\WindowsPowerShell\v1.0"
-echo %PATH% | findstr /C:"%newPath%" >nul
-if %errorLevel% == 0 (
+for /f "skip=2 tokens=1,2*" %%n in ('REG query "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" /v "Path" 2^>nul') do if /i "%%n" == "Path" set "PATH_M=%%p"
+setlocal enableDelayedExpansion
+for %%p in ("C:\Windows\system32" "C:\Windows\System32\WindowsPowerShell\v1.0") do (
+    echo "%PATH_M%" | find /i /v "%%~p;" | find /i /v "%%~p\;" >nul && set "newPath=!newPath!%%~p;"
+)
+if "%newPath%"=="" (
     echo Paths already exist in the "Path" variable.
     pause
     exit /b
 )
 
 REM Add paths to the system environment variable "Path"
-setx PATH "%newPath%;%PATH%" /M
+set "PATH_M=%newPath%%PATH_M%"
+setx PATH "%PATH_M:;;=;%" /M
 
 REM Display updated environment variables
 echo.
